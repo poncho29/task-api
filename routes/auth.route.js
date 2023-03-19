f542503d884f94cc064c2651a0c332cd40aca874
poncho29 <meneses0828@gmail.com>
2023-03-19 15:45:03 -0500

    [add] - generating session token with jwt

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,5 +1,8 @@
 const { Router } = require('express');
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+const { config } = require('../config');
 
 const router = Router();
 
@@ -7,7 +10,18 @@ router.post('/login',
   passport.authenticate('local', {session: false}),
   async (req, res, next) => {
     try {
-      res.json(req.user);
+      const user = req.user;
+
+      const payload = {
+        sub: user.id,
+      }
+
+      const token = jwt.sign(payload, config.jwtSecret);
+
+      res.json({
+        user,
+        token
+      });
     } catch (error) {
       next(error);
     }
